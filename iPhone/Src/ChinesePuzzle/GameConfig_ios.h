2aaf099101c3d79bc1bb5926855bede5b62d7cb7
Mathieu Ledru <matyo91@gmail.com>
2012-02-08 15:43:27 +0100

    iPhone Project

---
+++ iPhone/Src/ChinesePuzzle/GameConfig_ios.h
@@ -0,0 +1,37 @@
+/**
+ *  GameConfig_ios.h
+ *  ChinesePuzzle
+ *
+ *  Created by Mathieu LEDRU on 01/11/11.
+ *
+ *  GPL License:
+ *  Copyright (c) 2011, Mathieu LEDRU
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *  
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef __GAME_CONFIG_H__
+#define __GAME_CONFIG_H__
+
+#include "GameConfig.h"
+
+class CC_DLL GameConfig : public GameConfigCommon
+{
+public:
+	GameConfig();
+	virtual ~GameConfig();
+};
+ 
+#endif // __GAME_CONFIG_H__
