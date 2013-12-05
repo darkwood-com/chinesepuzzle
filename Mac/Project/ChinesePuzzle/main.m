4ef7b5a443f1e6b72b47d9ccafd50c5535f9c544
Mathieu Ledru <matyo91@gmail.com>
2013-12-05 13:10:35 +0100

    mac

---
+++ Mac/Project/ChinesePuzzle/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  ChinesePuzzle
+//
+//  Created by Mathieu Ledru on 05/12/2013.
+//  Copyright (c) 2013 Mathieu Ledru. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
