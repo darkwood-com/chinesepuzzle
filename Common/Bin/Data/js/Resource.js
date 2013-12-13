17cbd321e19e2f0baba55e814f19a1c4376b89e0
Mathieu Ledru <matyo91@gmail.com>
2013-12-13 18:22:34 +0100

    delete res

--- Common/Bin/Data/js/Resource.js
+++ Common/Bin/Data/js/Resource.js
@@ -1,12 +1,5 @@
-var s_HelloWorld = "res/HelloWorld.png";
-var s_CloseNormal = "res/CloseNormal.png";
-var s_CloseSelected = "res/CloseSelected.png";
-
 var g_ressources = [
     //image
-    {src:s_HelloWorld},
-    {src:s_CloseNormal},
-    {src:s_CloseSelected}
 
     //plist
 
