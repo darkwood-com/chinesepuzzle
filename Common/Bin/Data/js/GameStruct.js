a0623d885f85cc71041711d587652652c5d56c0b
Mathieu Ledru <matyo91@gmail.com>
2014-01-02 12:00:58 +0100

    Game

---
+++ Common/Bin/Data/js/GameStruct.js
@@ -0,0 +1,34 @@
+/*
+This file is part of the ChinesePuzzle package.
+
+(c) Mathieu Ledru
+
+For the full copyright and license information, please view the LICENSE
+file that was distributed with this source code.
+*/
+
+cpz.GridCoord = cc.Class.extend({
+  i: 0,
+  j: 0
+});
+
+cpz.gc = function(i, j) {
+  var coord;
+  coord = new cpz.GridCoord();
+  coord.i = i;
+  coord.j = j;
+  return coord;
+};
+
+cpz.MoveCoord = cc.Class.extend({
+  from: null,
+  to: null
+});
+
+cpz.mv = function(from, to) {
+  var coord;
+  coord = new cpz.MoveCoord();
+  coord.from = from;
+  coord.to = to;
+  return coord;
+};
