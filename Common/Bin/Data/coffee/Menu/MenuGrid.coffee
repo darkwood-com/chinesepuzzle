eb408edfe2a0e4c5edac847bfa08d1b655726a32
Mathieu Ledru <matyo91@gmail.com>
2014-01-15 16:52:45 +0100

    MenuGrid

--- Common/Bin/Data/coffee/Menu/MenuGrid.coffee
+++ Common/Bin/Data/coffee/Menu/MenuGrid.coffee
@@ -4,51 +4,242 @@ This file is part of the ChinesePuzzle package.
 (c) Mathieu Ledru
 
 For the full copyright and license information, please view the LICENSE
-file that was distributed with this source code.
+file that was distributed with @ source code.
 ###
 
-cpz.MenuBox = cc.Node.extend(
-  _bg: null
-  _validBtn: null
-  _titleLabel: null
+cpz.MenuGrid = cc.Node.extend(
+  _itemsGrid: null #items (=3x gridSize)
 
-  _state: null
-  _itemForTouch: (touch) ->
+  _resetGrid: ->
+    for coord in @_itemsGrid.allKeys()
+      item = @_itemsGrid.object(coord)
+      @removeChild(item, true)
+    @_itemsGrid.removeAllObjects()
+
+    a = @_gridSize.width * @_gridSize.height
+    pageMin = a * (page - 1)
+    pageMax = a * (page + 2) -1
+    for k in [pageMin..pageMax]
+      if(k >= 0 and k < @_items.count())
+        item = @_items[k]
+        item.setAnchorPoint cc.p(0.5, 0.5)
+        @addChild(item)
+
+        p = k / a
+        coord = cc.p(p * @_gridSize.width + k % @_gridSize.width, @_gridSize.height - 1 - (k - p * a) / @_gridSize.width)
+        @_itemsGrid.setObject item, coord
+
+    @layout()
+
+  #The x coord of initial point the user starts their swipe.
+  _startSwipe: 0
+
+  #Internal state of scroll (scrolling or idle).
+  _state: cpz.MenuGridScrollLayerState.Idle
+
+  #Holds the touch that started the scroll
+  _scrollTouch: null
+
+  _swipeToPageEnded: ->
+    @setPage @getPage() - Math.round(@getSwipe() / @getContentSize().width)
+
+    @_delegate.scrollLayerScrolledToPageNumber(@, @_page) if @_delegate
+    
+  _getMaxPage: ->
+    return Math.ceil(@_items.length / (@_gridSize.width * @_gridSize.height))
+
   _selectedItem: null
 
-  _layoutFontFile: null
-
-  _titlePosition: null
-  _validPosition: null
-
   _items: []
+  _gridSize: null
+  _offsetSwipe: null
+  _size: null
+
+  _page: null
+  _delegate: null
+  _minimumTouchLengthToSlide: null
+  _minimumTouchLengthToChangePage: null
 
   ctor: ->
     @_super()
 
-  initWithConf: (conf) ->
-  initWithConfAndContentSize: (conf, size) ->
+    @_itemsGrid = new cc.Dictionary()
+    @_gridSize = cc.size 0, 0
+    @_page = 0
+    @_delegate = null
+    @_startSwipe = 0
+    @_offsetSwipe = 0
+    @_scrollTouch = null
+    @_selectedItem = null
+    @_state = cpz.MenuGridScrollLayerState.Idle
 
-  getTitle: ->
-  setTitle: (title, fontFile) ->
-  getTitlePosition: -> @_titlePosition
-  setTitlePosition: (@_titlePosition) -> @
-  getValidPosition: -> @_validPosition
-  setValidPosition: (@_validPosition) -> @
+  init: ->
+    @_items = []
 
+    # Set default minimum touch length to scroll.
+    @_minimumTouchLengthToSlide = 10.5
+    @_minimumTouchLengthToChangePage = 100.5
+
+    true
+
+  initWithContentSize: (size) ->
+    if @init()
+      @setContentSize(size)
+  
+      # Set default minimum touch length to scroll.
+      @_minimumTouchLengthToChangePage = size.width / 8
+  
+      return true
+
+    false
+
+  getItems: -> @_items
+  setItems: (@_items) ->
+    @_resetGrid()
+    @
+  getGridSize: -> @_gridSize
+  setGridSize: (@_gridSize) ->
+    @_resetGrid()
+    @
+  getSwipe: -> @_offsetSwipe
+  setSwipe: (@_offsetSwipe) ->
+    @layout()
+    @
   setContentSize: (size) ->
+    @_super(size)
+    @layout()
+    @
 
   layout: (anim = true) ->
+    size = @getContentSize()
 
-  setOkTarget: (rec, selector) ->
+    if(@_gridSize.width > 0 and @_gridSize.height > 0)
+      pad = cc.size(size.width / @_gridSize.width, size.height / @_gridSize.height)
+      origin = cc.p(size.width / (2 * @_gridSize.width) - page * size.width, size.height / (2 * @_gridSize.height))
+
+      for coord in @_itemsGrid.allKeys()
+        item = @_itemsGrid.object(coord)
+        item.setPosition(cc.pAdd(origin, cc.p(coord.x * pad.width + @_offsetSwipe, coord.y * pad.height)))
+
+  visit: ->
+    #glEnable(GL_SCISSOR_TEST)
+
+    size = @getContentSize()
+    rect = cc.rect(0, 0, size.width, size.height)
+    rect.origin = @convertToWorldSpace(rect.origin)
+    cc.Director.getInstance()
+      .getOpenGLView()
+      .setScissorInPoints(rect.origin.x, rect.origin.y, rect.size.width, rect.size.height)
+
+    @_super()
+
+    #glDisable(GL_SCISSOR_TEST)
+
+  getPage: -> @_page
+  setPage: (page) ->
+    if(page >= 0 and page < @getMaxPage())
+      @_page = page
+  
+      @setSwipe(0)
+      @_resetGrid()
+
+    @
+
+  # Moves scrollLayer to page with given number & invokes delegate
+  # method scrollLayer:scrolledToPageNumber: at the end of CCMoveTo action.
+  # Does nothing if number >= totalScreens or < 0.
+  swipeToPage: (page) ->
+    if(page >= 0 and page < @getMaxPage())
+      @runAction(cc.Sequence.create([
+        #MenuGridSwipeTo::actionWithDuration(0.3, (@_page - page) * @getContentSize().width),
+        #CCCallFunc::actionWithTarget(@, callfunc_selector(MenuGrid::swipeToPageEnded))
+      ]))
+
+  getDelegate: -> @_delegate
+  setDelegate: (@_delegate) -> @
+  getMinimumTouchLengthToSlide: -> @_minimumTouchLengthToSlide
+  setMinimumTouchLengthToSlide: (@_minimumTouchLengthToSlide) -> @
+  getMinimumTouchLengthToChangePage: -> @_minimumTouchLengthToChangePage
+  setMinimumTouchLengthToChangePage: (@_minimumTouchLengthToChangePage) -> @
 
   onTouchBegan: (touch, event) ->
+    touchPoint = touch.locationInView()
+    touchPoint = cc.Director.getInstance().convertToGL(touchPoint)
+    
+    if (@_items and @_items.count() > 0)
+      for child in @_items
+        if child instanceof cc.MenuItem and child.isVisible() and child.isEnabled()
+          local = child.convertToNodeSpace(touchPoint)
+          r = child.rect()
+          r.origin = cc.PointZero()
+          
+          if cc.rectContainsPoint(r, local)
+            if (@_selectedItem)
+              @_selectedItem.unselected()
+            
+            @_selectedItem = child
+            @_selectedItem.selected()
+            @_selectedItem.runAction(cc.Sequence.create([
+              #CCEaseIn::actionWithAction(CCScaleTo::actionWithDuration(0.1, 0.75), 2.5),
+              #CCEaseOut::actionWithAction(CCScaleTo::actionWithDuration(0.1, 1.5), 2.5),
+            ]))
+    
+    if (!@_scrollTouch)
+      @_scrollTouch = touch
+    else
+      return false
+    
+    @_startSwipe = touchPoint.x
+    @_state = cpz.MenuGridScrollLayerState.Idle
+    
+    return true
+    
   onTouchMoved: (touch, event) ->
-  onTouchEnded: (touch, event) ->
-  onTouchCancelled: (touch, event) ->
+    return if(@_scrollTouch isnt touch)
+    
+    touchPoint = touch.locationInView()
+    touchPoint = cc.Director.getInstance().convertToGL(touchPoint)
+    
+    # If finger is dragged for more distance then minimum - start sliding and cancel pressed buttons.
+    # Of course only if we not already in sliding mode
+    if (@_state isnt cpz.MenuGridScrollLayerState.Sliding) and (fabsf(touchPoint.x - @_startSwipe) >= @_minimumTouchLengthToSlide)
+      @_state = cpz.MenuGridScrollLayerState.Sliding
+      
+      # Avoid jerk after state change.
+      @_startSwipe = touchPoint.x
 
-  setItems: (@_items) -> @
-  getItems: -> @_items
-  addItem: (child, zOrder, tag) ->
-  removeItemByTag: (tag, cleanup) ->
+      @_delegate.scrollLayerScrollingStarted(@) if @_delegate
+    
+    if @_state is cpz.MenuGridScrollLayerState.Sliding
+      @setSwipe(touchPoint.x - @_startSwipe)
+    
+  onTouchEnded: (touch, event) ->
+    return if(@_scrollTouch isnt touch)
+    
+    @_scrollTouch = null
+    
+    touchPoint = touch.locationInView()
+    touchPoint = cc.Director.getInstance().convertToGL(touchPoint)
+    
+    selectedPage = @getPage()
+    swipe = touchPoint.x - @_startSwipe
+    
+    @setSwipe(swipe)
+    if (swipe > 0 and swipe >= @_minimumTouchLengthToChangePage)
+      selectedPage -= ((@getSwipe() - @_minimumTouchLengthToChangePage) / @getContentSize().width) + 1
+    else if(swipe < 0 and swipe <= -@_minimumTouchLengthToChangePage)
+      selectedPage += - ((@getSwipe() + @_minimumTouchLengthToChangePage) / @getContentSize().width) + 1
+    if(selectedPage < 0) then selectedPage = 0
+    if(selectedPage >= @getMaxPage()) then selectedPage = @getMaxPage() - 1
+    
+    @swipeToPage(selectedPage)
+    
+  onTouchCancelled: (touch, event) ->
+    if @_scrollTouch is touch
+      @_scrollTouch = null
+      @setPage(@_page)
 )
+
+cpz.MenuGridScrollLayerState =
+  Idle: 0
+  Sliding: 1
--- Common/Bin/Data/js/Menu/MenuGrid.js
+++ Common/Bin/Data/js/Menu/MenuGrid.js
@@ -4,59 +4,258 @@ This file is part of the ChinesePuzzle package.
 (c) Mathieu Ledru
 
 For the full copyright and license information, please view the LICENSE
-file that was distributed with this source code.
+file that was distributed with @ source code.
 */
 
-cpz.MenuBox = cc.Node.extend({
-  _bg: null,
-  _validBtn: null,
-  _titleLabel: null,
-  _state: null,
-  _itemForTouch: function(touch) {},
-  _selectedItem: null,
-  _layoutFontFile: null,
-  _titlePosition: null,
-  _validPosition: null,
-  _items: [],
-  ctor: function() {
-    return this._super();
+cpz.MenuGrid = cc.Node.extend({
+  _itemsGrid: null,
+  _resetGrid: function() {
+    var a, coord, item, k, p, pageMax, pageMin, _i, _j, _len, _ref;
+    _ref = this._itemsGrid.allKeys();
+    for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+      coord = _ref[_i];
+      item = this._itemsGrid.object(coord);
+      this.removeChild(item, true);
+    }
+    this._itemsGrid.removeAllObjects();
+    a = this._gridSize.width * this._gridSize.height;
+    pageMin = a * (page - 1);
+    pageMax = a * (page + 2)(-1);
+    for (k = _j = pageMin; pageMin <= pageMax ? _j <= pageMax : _j >= pageMax; k = pageMin <= pageMax ? ++_j : --_j) {
+      if (k >= 0 && k < this._items.count()) {
+        item = this._items[k];
+        item.setAnchorPoint(cc.p(0.5, 0.5));
+        this.addChild(item);
+        p = k / a;
+        coord = cc.p(p * this._gridSize.width + k % this._gridSize.width, this._gridSize.height - 1 - (k - p * a) / this._gridSize.width);
+        this._itemsGrid.setObject(item, coord);
+      }
+    }
+    return this.layout();
   },
-  initWithConf: function(conf) {},
-  initWithConfAndContentSize: function(conf, size) {},
-  getTitle: function() {},
-  setTitle: function(title, fontFile) {},
-  getTitlePosition: function() {
-    return this._titlePosition;
-  },
-  setTitlePosition: function(_titlePosition) {
-    this._titlePosition = _titlePosition;
-    return this;
-  },
-  getValidPosition: function() {
-    return this._validPosition;
-  },
-  setValidPosition: function(_validPosition) {
-    this._validPosition = _validPosition;
-    return this;
-  },
-  setContentSize: function(size) {},
-  layout: function(anim) {
-    if (anim == null) {
-      anim = true;
+  _startSwipe: 0,
+  _state: cpz.MenuGridScrollLayerState.Idle,
+  _scrollTouch: null,
+  _swipeToPageEnded: function() {
+    this.setPage(this.getPage() - Math.round(this.getSwipe() / this.getContentSize().width));
+    if (this._delegate) {
+      return this._delegate.scrollLayerScrolledToPageNumber(this, this._page);
     }
   },
-  setOkTarget: function(rec, selector) {},
-  onTouchBegan: function(touch, event) {},
-  onTouchMoved: function(touch, event) {},
-  onTouchEnded: function(touch, event) {},
-  onTouchCancelled: function(touch, event) {},
-  setItems: function(_items) {
-    this._items = _items;
-    return this;
+  _getMaxPage: function() {
+    return Math.ceil(this._items.length / (this._gridSize.width * this._gridSize.height));
+  },
+  _selectedItem: null,
+  _items: [],
+  _gridSize: null,
+  _offsetSwipe: null,
+  _size: null,
+  _page: null,
+  _delegate: null,
+  _minimumTouchLengthToSlide: null,
+  _minimumTouchLengthToChangePage: null,
+  ctor: function() {
+    this._super();
+    this._itemsGrid = new cc.Dictionary();
+    this._gridSize = cc.size(0, 0);
+    this._page = 0;
+    this._delegate = null;
+    this._startSwipe = 0;
+    this._offsetSwipe = 0;
+    this._scrollTouch = null;
+    this._selectedItem = null;
+    return this._state = cpz.MenuGridScrollLayerState.Idle;
+  },
+  init: function() {
+    this._items = [];
+    this._minimumTouchLengthToSlide = 10.5;
+    this._minimumTouchLengthToChangePage = 100.5;
+    return true;
+  },
+  initWithContentSize: function(size) {
+    if (this.init()) {
+      this.setContentSize(size);
+      this._minimumTouchLengthToChangePage = size.width / 8;
+      return true;
+    }
+    return false;
   },
   getItems: function() {
     return this._items;
   },
-  addItem: function(child, zOrder, tag) {},
-  removeItemByTag: function(tag, cleanup) {}
+  setItems: function(_items) {
+    this._items = _items;
+    this._resetGrid();
+    return this;
+  },
+  getGridSize: function() {
+    return this._gridSize;
+  },
+  setGridSize: function(_gridSize) {
+    this._gridSize = _gridSize;
+    this._resetGrid();
+    return this;
+  },
+  getSwipe: function() {
+    return this._offsetSwipe;
+  },
+  setSwipe: function(_offsetSwipe) {
+    this._offsetSwipe = _offsetSwipe;
+    this.layout();
+    return this;
+  },
+  setContentSize: function(size) {
+    this._super(size);
+    this.layout();
+    return this;
+  },
+  layout: function(anim) {
+    var coord, item, origin, pad, size, _i, _len, _ref, _results;
+    if (anim == null) {
+      anim = true;
+    }
+    size = this.getContentSize();
+    if (this._gridSize.width > 0 && this._gridSize.height > 0) {
+      pad = cc.size(size.width / this._gridSize.width, size.height / this._gridSize.height);
+      origin = cc.p(size.width / (2 * this._gridSize.width) - page * size.width, size.height / (2 * this._gridSize.height));
+      _ref = this._itemsGrid.allKeys();
+      _results = [];
+      for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+        coord = _ref[_i];
+        item = this._itemsGrid.object(coord);
+        _results.push(item.setPosition(cc.pAdd(origin, cc.p(coord.x * pad.width + this._offsetSwipe, coord.y * pad.height))));
+      }
+      return _results;
+    }
+  },
+  visit: function() {
+    var rect, size;
+    size = this.getContentSize();
+    rect = cc.rect(0, 0, size.width, size.height);
+    rect.origin = this.convertToWorldSpace(rect.origin);
+    cc.Director.getInstance().getOpenGLView().setScissorInPoints(rect.origin.x, rect.origin.y, rect.size.width, rect.size.height);
+    return this._super();
+  },
+  getPage: function() {
+    return this._page;
+  },
+  setPage: function(page) {
+    if (page >= 0 && page < this.getMaxPage()) {
+      this._page = page;
+      this.setSwipe(0);
+      this._resetGrid();
+    }
+    return this;
+  },
+  swipeToPage: function(page) {
+    if (page >= 0 && page < this.getMaxPage()) {
+      return this.runAction(cc.Sequence.create([]));
+    }
+  },
+  getDelegate: function() {
+    return this._delegate;
+  },
+  setDelegate: function(_delegate) {
+    this._delegate = _delegate;
+    return this;
+  },
+  getMinimumTouchLengthToSlide: function() {
+    return this._minimumTouchLengthToSlide;
+  },
+  setMinimumTouchLengthToSlide: function(_minimumTouchLengthToSlide) {
+    this._minimumTouchLengthToSlide = _minimumTouchLengthToSlide;
+    return this;
+  },
+  getMinimumTouchLengthToChangePage: function() {
+    return this._minimumTouchLengthToChangePage;
+  },
+  setMinimumTouchLengthToChangePage: function(_minimumTouchLengthToChangePage) {
+    this._minimumTouchLengthToChangePage = _minimumTouchLengthToChangePage;
+    return this;
+  },
+  onTouchBegan: function(touch, event) {
+    var child, local, r, touchPoint, _i, _len, _ref;
+    touchPoint = touch.locationInView();
+    touchPoint = cc.Director.getInstance().convertToGL(touchPoint);
+    if (this._items && this._items.count() > 0) {
+      _ref = this._items;
+      for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+        child = _ref[_i];
+        if (child instanceof cc.MenuItem && child.isVisible() && child.isEnabled()) {
+          local = child.convertToNodeSpace(touchPoint);
+          r = child.rect();
+          r.origin = cc.PointZero();
+          if (cc.rectContainsPoint(r, local)) {
+            if (this._selectedItem) {
+              this._selectedItem.unselected();
+            }
+            this._selectedItem = child;
+            this._selectedItem.selected();
+            this._selectedItem.runAction(cc.Sequence.create([]));
+          }
+        }
+      }
+    }
+    if (!this._scrollTouch) {
+      this._scrollTouch = touch;
+    } else {
+      return false;
+    }
+    this._startSwipe = touchPoint.x;
+    this._state = cpz.MenuGridScrollLayerState.Idle;
+    return true;
+  },
+  onTouchMoved: function(touch, event) {
+    var touchPoint;
+    if (this._scrollTouch !== touch) {
+      return;
+    }
+    touchPoint = touch.locationInView();
+    touchPoint = cc.Director.getInstance().convertToGL(touchPoint);
+    if ((this._state !== cpz.MenuGridScrollLayerState.Sliding) && (fabsf(touchPoint.x - this._startSwipe) >= this._minimumTouchLengthToSlide)) {
+      this._state = cpz.MenuGridScrollLayerState.Sliding;
+      this._startSwipe = touchPoint.x;
+      if (this._delegate) {
+        this._delegate.scrollLayerScrollingStarted(this);
+      }
+    }
+    if (this._state === cpz.MenuGridScrollLayerState.Sliding) {
+      return this.setSwipe(touchPoint.x - this._startSwipe);
+    }
+  },
+  onTouchEnded: function(touch, event) {
+    var selectedPage, swipe, touchPoint;
+    if (this._scrollTouch !== touch) {
+      return;
+    }
+    this._scrollTouch = null;
+    touchPoint = touch.locationInView();
+    touchPoint = cc.Director.getInstance().convertToGL(touchPoint);
+    selectedPage = this.getPage();
+    swipe = touchPoint.x - this._startSwipe;
+    this.setSwipe(swipe);
+    if (swipe > 0 && swipe >= this._minimumTouchLengthToChangePage) {
+      selectedPage -= ((this.getSwipe() - this._minimumTouchLengthToChangePage) / this.getContentSize().width) + 1;
+    } else if (swipe < 0 && swipe <= -this._minimumTouchLengthToChangePage) {
+      selectedPage += -((this.getSwipe() + this._minimumTouchLengthToChangePage) / this.getContentSize().width) + 1;
+    }
+    if (selectedPage < 0) {
+      selectedPage = 0;
+    }
+    if (selectedPage >= this.getMaxPage()) {
+      selectedPage = this.getMaxPage() - 1;
+    }
+    return this.swipeToPage(selectedPage);
+  },
+  onTouchCancelled: function(touch, event) {
+    if (this._scrollTouch === touch) {
+      this._scrollTouch = null;
+      return this.setPage(this._page);
+    }
+  }
 });
+
+cpz.MenuGridScrollLayerState = {
+  Idle: 0,
+  Sliding: 1
+};
