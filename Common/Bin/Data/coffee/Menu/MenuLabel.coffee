9e1be351deb8234674d1605326a89d791f510970
Mathieu Ledru <matyo91@gmail.com>
2014-01-16 16:29:52 +0100

    MenuLabel

--- Common/Bin/Data/coffee/Menu/MenuLabel.coffee
+++ Common/Bin/Data/coffee/Menu/MenuLabel.coffee
@@ -4,13 +4,11 @@ This file is part of the ChinesePuzzle package.
 (c) Mathieu Ledru
 
 For the full copyright and license information, please view the LICENSE
-file that was distributed with this source code.
+file that was distributed with @ source code.
 ###
 
 cpz.MenuLabel = cc.Node.extend(
   _label: null
-
-  _resetLabel: ->
 
   _startSwipe: null
   _state: null
@@ -22,24 +20,118 @@ cpz.MenuLabel = cc.Node.extend(
   ctor: ->
     @_super()
 
-  initWithContentSizeAndFntFile: (size, fntFile) ->
+    @_label = null
+    @_startSwipe = 0
+    @_offsetSwipe = 0
+    @_offsetScroll = 0
+    @_scrollTouch = null
+    @_state = cpz.MenuLabelScrollLayerState.Idle
 
+  initWithContentSizeAndFntFile: (size, fntFile) ->
+    @setContentSize size
+    
+    @_label = new cc.LabelBMFont()
+    @_label.initWithString("", fntFile, 0, cc.TEXT_ALIGNMENT_LEFT)
+    @_label.setAnchorPoint(cc.p(0.5, 1.0))
+    @addChild(@_label)
+    
+    return true
+
+  getString: -> @_label.getString()
   setString: (str) ->
-  getString: ->
+    @_label.setString(str)
+    @
+
+  getWidth: -> @_label.getContentSize().width
   setWidth: (width) ->
-  getWidth: ->
+    @_label.setWidth(width)
+    @
+    
   setAlignment: (alignment) ->
+    @_label.setAlignment(alignment)
+    @
+    
   getOffsetSwipe: -> @_offsetSwipe
-  setOffsetSwipe: (@_offsetSwipe) -> @
+  setOffsetSwipe: (offsetSwipe) ->
+    @_offsetSwipe = offsetSwipe
+    
+    @layout()
+    @
+    
   getOffsetScroll: -> @_offsetScroll
   setOffsetScroll: (@_offsetScroll) -> @
+  
   setContentSize: (size) ->
+    @_super(size)
+    
+    @layout()
+    @
 
   layout: (anim = true) ->
+    size = @getContentSize()
+    
+    if(@_label)
+      @_label.setPosition(cc.pAdd(cc.p(size.width / 2, size.height), cc.p(0, @_offsetScroll + @_offsetSwipe)))
+    
   visit: ->
+    #glEnable(GL_SCISSOR_TEST)
+    
+    #CCSize size = @getContentSize()
+    #CCRect rect = CCRectMake(0, 0, size.width, size.height)
+    #rect.origin = @convertToWorldSpace(rect.origin)
+    #cc.Director.getInstance().getOpenGLView().setScissorInPoints(rect.origin.x, rect.origin.y, rect.size.width, rect.size.height)
+    
+    @_super()
+    
+    #glDisable(GL_SCISSOR_TEST)
 
   onTouchBegan: (touch, event) ->
+    if not @_scrollTouch
+      @_scrollTouch = touch
+    else
+      return false
+    
+    touchPoint = touch.locationInView()
+    touchPoint = cc.Director.getInstance().convertToGL(touchPoint)
+    
+    @_startSwipe = touchPoint.y
+    @_state = cpz.MenuLabelScrollLayerState.Idle
+    
+    true
+    
   onTouchMoved: (touch, event) ->
+    return if @_scrollTouch isnt touch
+    
+    touchPoint = touch.locationInView()
+    touchPoint = cc.Director.getInstance().convertToGL(touchPoint)
+    
+    # If finger is dragged for more distance then minimum - start sliding and cancel pressed buttons.
+    # Of course only if we not already in sliding mode
+    if @_state isnt cpz.MenuLabelScrollLayerState.Sliding
+      @_state = cpz.MenuLabelScrollLayerState.Sliding
+      
+      # Avoid jerk after state change.
+      @_startSwipe = touchPoint.y
+    
+    if @_state is cpz.MenuLabelScrollLayerState.Sliding
+      @setSwipe(touchPoint.y - @_startSwipe)
+    
   onTouchEnded: (touch, event) ->
+    return if @_scrollTouch isnt touch
+    
+    @_scrollTouch = null
+    
+    touchPoint = touch.locationInView()
+    touchPoint = cc.Director.getInstance().convertToGL(touchPoint)
+    
+    @setScroll(@getScroll() + touchPoint.y - @_startSwipe)
+    @setSwipe(0)
+    
   onTouchCancelled: (touch, event) ->
+    if @_scrollTouch is touch
+      @_scrollTouch = null
 )
+
+cpz.MenuLabelScrollLayerState =
+  Idle: 0
+  Sliding: 1
--- Common/Bin/Data/js/Menu/MenuLabel.js
+++ Common/Bin/Data/js/Menu/MenuLabel.js
@@ -4,31 +4,57 @@ This file is part of the ChinesePuzzle package.
 (c) Mathieu Ledru
 
 For the full copyright and license information, please view the LICENSE
-file that was distributed with this source code.
+file that was distributed with @ source code.
 */
 
 cpz.MenuLabel = cc.Node.extend({
   _label: null,
-  _resetLabel: function() {},
   _startSwipe: null,
   _state: null,
   _scrollTouch: null,
   _offsetSwipe: null,
   _offsetScroll: null,
   ctor: function() {
-    return this._super();
+    this._super();
+    this._label = null;
+    this._startSwipe = 0;
+    this._offsetSwipe = 0;
+    this._offsetScroll = 0;
+    this._scrollTouch = null;
+    return this._state = cpz.MenuLabelScrollLayerState.Idle;
   },
-  initWithContentSizeAndFntFile: function(size, fntFile) {},
-  setString: function(str) {},
-  getString: function() {},
-  setWidth: function(width) {},
-  getWidth: function() {},
-  setAlignment: function(alignment) {},
+  initWithContentSizeAndFntFile: function(size, fntFile) {
+    this.setContentSize(size);
+    this._label = new cc.LabelBMFont();
+    this._label.initWithString("", fntFile, 0, cc.TEXT_ALIGNMENT_LEFT);
+    this._label.setAnchorPoint(cc.p(0.5, 1.0));
+    this.addChild(this._label);
+    return true;
+  },
+  getString: function() {
+    return this._label.getString();
+  },
+  setString: function(str) {
+    this._label.setString(str);
+    return this;
+  },
+  getWidth: function() {
+    return this._label.getContentSize().width;
+  },
+  setWidth: function(width) {
+    this._label.setWidth(width);
+    return this;
+  },
+  setAlignment: function(alignment) {
+    this._label.setAlignment(alignment);
+    return this;
+  },
   getOffsetSwipe: function() {
     return this._offsetSwipe;
   },
-  setOffsetSwipe: function(_offsetSwipe) {
-    this._offsetSwipe = _offsetSwipe;
+  setOffsetSwipe: function(offsetSwipe) {
+    this._offsetSwipe = offsetSwipe;
+    this.layout();
     return this;
   },
   getOffsetScroll: function() {
@@ -38,15 +64,71 @@ cpz.MenuLabel = cc.Node.extend({
     this._offsetScroll = _offsetScroll;
     return this;
   },
-  setContentSize: function(size) {},
+  setContentSize: function(size) {
+    this._super(size);
+    this.layout();
+    return this;
+  },
   layout: function(anim) {
+    var size;
     if (anim == null) {
       anim = true;
     }
+    size = this.getContentSize();
+    if (this._label) {
+      return this._label.setPosition(cc.pAdd(cc.p(size.width / 2, size.height), cc.p(0, this._offsetScroll + this._offsetSwipe)));
+    }
   },
-  visit: function() {},
-  onTouchBegan: function(touch, event) {},
-  onTouchMoved: function(touch, event) {},
-  onTouchEnded: function(touch, event) {},
-  onTouchCancelled: function(touch, event) {}
+  visit: function() {
+    return this._super();
+  },
+  onTouchBegan: function(touch, event) {
+    var touchPoint;
+    if (!this._scrollTouch) {
+      this._scrollTouch = touch;
+    } else {
+      return false;
+    }
+    touchPoint = touch.locationInView();
+    touchPoint = cc.Director.getInstance().convertToGL(touchPoint);
+    this._startSwipe = touchPoint.y;
+    this._state = cpz.MenuLabelScrollLayerState.Idle;
+    return true;
+  },
+  onTouchMoved: function(touch, event) {
+    var touchPoint;
+    if (this._scrollTouch !== touch) {
+      return;
+    }
+    touchPoint = touch.locationInView();
+    touchPoint = cc.Director.getInstance().convertToGL(touchPoint);
+    if (this._state !== cpz.MenuLabelScrollLayerState.Sliding) {
+      this._state = cpz.MenuLabelScrollLayerState.Sliding;
+      this._startSwipe = touchPoint.y;
+    }
+    if (this._state === cpz.MenuLabelScrollLayerState.Sliding) {
+      return this.setSwipe(touchPoint.y - this._startSwipe);
+    }
+  },
+  onTouchEnded: function(touch, event) {
+    var touchPoint;
+    if (this._scrollTouch !== touch) {
+      return;
+    }
+    this._scrollTouch = null;
+    touchPoint = touch.locationInView();
+    touchPoint = cc.Director.getInstance().convertToGL(touchPoint);
+    this.setScroll(this.getScroll() + touchPoint.y - this._startSwipe);
+    return this.setSwipe(0);
+  },
+  onTouchCancelled: function(touch, event) {
+    if (this._scrollTouch === touch) {
+      return this._scrollTouch = null;
+    }
+  }
 });
+
+cpz.MenuLabelScrollLayerState = {
+  Idle: 0,
+  Sliding: 1
+};
