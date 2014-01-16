5fb4fa53be9a4c9a5ef908945179ea064906337c
Mathieu Ledru <matyo91@gmail.com>
2014-01-16 16:36:54 +0100

    MenuLabelContainer

--- Common/Bin/Data/coffee/Menu/MenuLabelContainer.coffee
+++ Common/Bin/Data/coffee/Menu/MenuLabelContainer.coffee
@@ -14,22 +14,70 @@ cpz.MenuLabelContainer = cpz.MenuBox.extend(
 
   ctor: ->
     @_super()
+    
+    @_container = null
 
   initWithConfAndContentSizeAndFntFile: (conf, size, fntFile) ->
+    return false unless @_super(conf, size)
+    
+    @_container = new cpz.MenuLabel()
+    @_container.initWithContentSizeAndFntFile(size, fntFile)
+    @_container.setAnchorPoint(cc.p(0.5, 0.5))
+    @addChild(@_container)
+    
+    @_margin = cc.size(0, 0)
+    
+    return true
 
-  getString: ->
+  getString: -> @_container.getString()
   setString: (str) ->
-  getWidth: ->
+    @_container.setString(str)
+    @
+    
+  getWidth: -> @_container.getWidth()
   setWidth: (width) ->
+    @_container.setWidth(width)
+    @
+    
   setAlignment: (alignment) ->
+    @_container.setAlignment(alignment)
+    @
 
   getMargin: -> @_margin
-  setMargin: (@_margin) -> @
+  setMargin: (@_margin) ->
+    @_margin = margin
+    
+    @layout()
+    @
 
   layout: (anim = true) ->
+    @_super(anim)
+    
+    size = @getContentSize()
+    
+    if(@_container)
+      @_container.setPosition(cc.p(size.width / 2, size.height / 2))
+      @_container.setContentSize(cc.size(size.width - 2 * margin.width, size.height - 2 * margin.height))
+      @_container.setWidth(size.width - 2 * margin.width)
 
   onTouchBegan: (touch, event) ->
+    return false unless @_super(touch, event)
+    
+    return @_container.ccTouchBegan(touch, event)
+  
   onTouchMoved: (touch, event) ->
+    @_super(touch, event)
+    
+    @_container.ccTouchMoved(touch, event)
+
   onTouchEnded: (touch, event) ->
+    @_super(touch, event)
+    
+    @_container.ccTouchEnded(touch, event)
+  
   onTouchCancelled: (touch, event) ->
+    @_super(touch, event)
+      
+    @_container.ccTouchCancelled(touch, event)
+
 )
--- Common/Bin/Data/js/Menu/MenuLabelContainer.js
+++ Common/Bin/Data/js/Menu/MenuLabelContainer.js
@@ -11,28 +11,76 @@ cpz.MenuLabelContainer = cpz.MenuBox.extend({
   _container: null,
   _margin: null,
   ctor: function() {
-    return this._super();
+    this._super();
+    return this._container = null;
   },
-  initWithConfAndContentSizeAndFntFile: function(conf, size, fntFile) {},
-  getString: function() {},
-  setString: function(str) {},
-  getWidth: function() {},
-  setWidth: function(width) {},
-  setAlignment: function(alignment) {},
+  initWithConfAndContentSizeAndFntFile: function(conf, size, fntFile) {
+    if (!this._super(conf, size)) {
+      return false;
+    }
+    this._container = new cpz.MenuLabel();
+    this._container.initWithContentSizeAndFntFile(size, fntFile);
+    this._container.setAnchorPoint(cc.p(0.5, 0.5));
+    this.addChild(this._container);
+    this._margin = cc.size(0, 0);
+    return true;
+  },
+  getString: function() {
+    return this._container.getString();
+  },
+  setString: function(str) {
+    this._container.setString(str);
+    return this;
+  },
+  getWidth: function() {
+    return this._container.getWidth();
+  },
+  setWidth: function(width) {
+    this._container.setWidth(width);
+    return this;
+  },
+  setAlignment: function(alignment) {
+    this._container.setAlignment(alignment);
+    return this;
+  },
   getMargin: function() {
     return this._margin;
   },
   setMargin: function(_margin) {
     this._margin = _margin;
+    this._margin = margin;
+    this.layout();
     return this;
   },
   layout: function(anim) {
+    var size;
     if (anim == null) {
       anim = true;
     }
+    this._super(anim);
+    size = this.getContentSize();
+    if (this._container) {
+      this._container.setPosition(cc.p(size.width / 2, size.height / 2));
+      this._container.setContentSize(cc.size(size.width - 2 * margin.width, size.height - 2 * margin.height));
+      return this._container.setWidth(size.width - 2 * margin.width);
+    }
   },
-  onTouchBegan: function(touch, event) {},
-  onTouchMoved: function(touch, event) {},
-  onTouchEnded: function(touch, event) {},
-  onTouchCancelled: function(touch, event) {}
+  onTouchBegan: function(touch, event) {
+    if (!this._super(touch, event)) {
+      return false;
+    }
+    return this._container.ccTouchBegan(touch, event);
+  },
+  onTouchMoved: function(touch, event) {
+    this._super(touch, event);
+    return this._container.ccTouchMoved(touch, event);
+  },
+  onTouchEnded: function(touch, event) {
+    this._super(touch, event);
+    return this._container.ccTouchEnded(touch, event);
+  },
+  onTouchCancelled: function(touch, event) {
+    this._super(touch, event);
+    return this._container.ccTouchCancelled(touch, event);
+  }
 });
