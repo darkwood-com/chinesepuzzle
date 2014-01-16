b2d5fbb7b83c90c37b2793eba6489708d753aaaa
Mathieu Ledru <matyo91@gmail.com>
2014-01-16 12:21:20 +0100

    DecoratedBox

--- Common/Bin/Data/coffee/Cocos2D/DecoratedBox.coffee
+++ Common/Bin/Data/coffee/Cocos2D/DecoratedBox.coffee
@@ -8,5 +8,128 @@ file that was distributed with this source code.
 ###
 
 cpz.DecoratedBox = cc.Node.extend(
+  _cell: null
+  
+  _boxWidth: null
+  _boxHeight: null
+  
+  getCell: -> @_cell
+  getBoxWidth: -> @_boxWidth
+  getBoxHeight: -> @_boxHeight
 
+  initWithTexture: (texture, rect, size) ->
+    return false unless @_super(texture, 9)
+    
+    @_cell.origin = rect.origin
+    @_cell = cc.size(rect.width / 3, rect.height / 3)
+    @setAnchorPoint cc.p(0.5, 0.5)
+    @setContentSize size
+    
+    return true
+    
+  initWithFile: (filename, size) ->
+    return false unless @_super(filename, 9)
+    
+    textureSize = @getTextureAtlas().getTexture().getContentSize()
+    
+    @_cell.origin = cc.p(0, 0)
+    @_cell = cc.size(textureSize.width / 3, textureSize.height / 3)
+    @setAnchorPoint(cc.p(0.5, 0.5))
+    @setContentSize(size)
+    
+    return true
+
+  setContentSize: (size) ->
+    @removeAllChildrenWithCleanup(true)
+
+    @_super size
+
+    uw = Math.ceil(size.width / @_cell.width)
+    uh = Math.ceil(size.height / @_cell.height)
+    offw = size.width % @_cell.width
+    offh = size.height % @_cell.height
+
+    for j in [0..uh-1]
+      for i in [0..uw-1]
+  
+        rect = cc.rect()
+  
+        if i is (uw - 2) and j is (uw - 2)
+          rect = cc.rect(@_cell.width, @_cell.height, offw, offh)
+        else if i is (uw - 2)
+          if j is (uh - 1)
+            # Top border
+            rect = cc.rect(@_cell.width, 0, offw, @_cell.height)
+          else if j is 0
+            # Bottom border
+            rect = cc.rect(@_cell.width, @_cell.height * 2, offw, @_cell.height)
+          else
+            # Middle
+            rect = cc.rect(@_cell.width, @_cell.height, offw, @_cell.height)
+        else if j is (uw - 2)
+          if i is (uh - 1)
+            # Right border
+            rect = cc.rect(@_cell.width * 2, @_cell.height, @_cell.width, offh)
+          else if i is 0
+            # Left border
+            rect = cc.rect(0, @_cell.height, @_cell.width, offh)
+          else
+            # Middle
+            rect = cc.rect(@_cell.width, @_cell.height, @_cell.width, offh)
+        else if i is 0
+          if j is (uh - 1)
+            # Top left cap
+            rect = cc.rect(0, 0, @_cell.width, @_cell.height)
+          else if j is 0
+            # Bottom left cap
+            rect = cc.rect(0, @_cell.height * 2, @_cell.width, @_cell.height)
+          else
+            # Left border
+            rect = cc.rect(0, @_cell.height, @_cell.width, @_cell.height)
+        else if i is (uw - 1)
+          if j is (uh - 1)
+            # Top right cap
+            rect = cc.rect(@_cell.width * 2, 0, @_cell.width, @_cell.height)
+          else if j is 0
+            # Bottom right cap
+            rect = cc.rect(@_cell.width * 2, @_cell.height * 2, @_cell.width, @_cell.height)
+          else
+            # Right border
+            rect = cc.rect(@_cell.width * 2, @_cell.height, @_cell.width, @_cell.height)
+        else if j is (uh - 1)
+          # Top border
+          rect = cc.rect(@_cell.width, 0, @_cell.width, @_cell.height)
+        else if j is 0
+          # Bottom border
+          rect = cc.rect(@_cell.width, @_cell.height * 2, @_cell.width, @_cell.height)
+        else
+          # Middle
+          rect = cc.rect(@_cell.width, @_cell.height, @_cell.width, @_cell.height)
+      
+        rect.origin = cc.pAdd(rect.origin, @_cell.origin)
+        
+        b = cc.Sprite.createWithTexture(@getTexture(), rect)
+        b.setAnchorPoint(cc.p(0, 0))
+        if j is (uh - 1) and i is (uw - 1)
+          b.setPosition(cc.p((i - 1) * @_cell.width + offw, (j - 1) * @_cell.height + offh))
+        else if j is (uh - 1)
+          b.setPosition(cc.p(i * @_cell.width, (j - 1) * @_cell.height + offh))
+        else if i is (uw - 1)
+          b.setPosition(cc.p((i - 1) * @_cell.width + offw, j * @_cell.height))
+        else
+          b.setPosition(cc.p(i * @_cell.width, j * @_cell.height))
+        b.setTag(j * @_cell.height + i)
+        
+        @addChild(b)
+    
 )
+
+cpz.DecoratedBox.createWithTexture = (texture, rect, size) ->
+  box = new DecoratedBox()
+  return box if box and box.initWithTexture(texture, rect, size)
+  null
+
+cpz.DecoratedBox.createWithFile = (filename, size) ->
+  box = new DecoratedBox()
+  return box if box and box.initWithFile(filename, size)
+  null
--- Common/Bin/Data/js/Cocos2D/DecoratedBox.js
+++ Common/Bin/Data/js/Cocos2D/DecoratedBox.js
@@ -7,4 +7,133 @@ For the full copyright and license information, please view the LICENSE
 file that was distributed with this source code.
 */
 
-cpz.DecoratedBox = cc.Node.extend();
+cpz.DecoratedBox = cc.Node.extend({
+  _cell: null,
+  _boxWidth: null,
+  _boxHeight: null,
+  getCell: function() {
+    return this._cell;
+  },
+  getBoxWidth: function() {
+    return this._boxWidth;
+  },
+  getBoxHeight: function() {
+    return this._boxHeight;
+  },
+  initWithTexture: function(texture, rect, size) {
+    if (!this._super(texture, 9)) {
+      return false;
+    }
+    this._cell.origin = rect.origin;
+    this._cell = cc.size(rect.width / 3, rect.height / 3);
+    this.setAnchorPoint(cc.p(0.5, 0.5));
+    this.setContentSize(size);
+    return true;
+  },
+  initWithFile: function(filename, size) {
+    var textureSize;
+    if (!this._super(filename, 9)) {
+      return false;
+    }
+    textureSize = this.getTextureAtlas().getTexture().getContentSize();
+    this._cell.origin = cc.p(0, 0);
+    this._cell = cc.size(textureSize.width / 3, textureSize.height / 3);
+    this.setAnchorPoint(cc.p(0.5, 0.5));
+    this.setContentSize(size);
+    return true;
+  },
+  setContentSize: function(size) {
+    var b, i, j, offh, offw, rect, uh, uw, _i, _ref, _results;
+    this.removeAllChildrenWithCleanup(true);
+    this._super(size);
+    uw = Math.ceil(size.width / this._cell.width);
+    uh = Math.ceil(size.height / this._cell.height);
+    offw = size.width % this._cell.width;
+    offh = size.height % this._cell.height;
+    _results = [];
+    for (j = _i = 0, _ref = uh - 1; 0 <= _ref ? _i <= _ref : _i >= _ref; j = 0 <= _ref ? ++_i : --_i) {
+      _results.push((function() {
+        var _j, _ref1, _results1;
+        _results1 = [];
+        for (i = _j = 0, _ref1 = uw - 1; 0 <= _ref1 ? _j <= _ref1 : _j >= _ref1; i = 0 <= _ref1 ? ++_j : --_j) {
+          rect = cc.rect();
+          if (i === (uw - 2) && j === (uw - 2)) {
+            rect = cc.rect(this._cell.width, this._cell.height, offw, offh);
+          } else if (i === (uw - 2)) {
+            if (j === (uh - 1)) {
+              rect = cc.rect(this._cell.width, 0, offw, this._cell.height);
+            } else if (j === 0) {
+              rect = cc.rect(this._cell.width, this._cell.height * 2, offw, this._cell.height);
+            } else {
+              rect = cc.rect(this._cell.width, this._cell.height, offw, this._cell.height);
+            }
+          } else if (j === (uw - 2)) {
+            if (i === (uh - 1)) {
+              rect = cc.rect(this._cell.width * 2, this._cell.height, this._cell.width, offh);
+            } else if (i === 0) {
+              rect = cc.rect(0, this._cell.height, this._cell.width, offh);
+            } else {
+              rect = cc.rect(this._cell.width, this._cell.height, this._cell.width, offh);
+            }
+          } else if (i === 0) {
+            if (j === (uh - 1)) {
+              rect = cc.rect(0, 0, this._cell.width, this._cell.height);
+            } else if (j === 0) {
+              rect = cc.rect(0, this._cell.height * 2, this._cell.width, this._cell.height);
+            } else {
+              rect = cc.rect(0, this._cell.height, this._cell.width, this._cell.height);
+            }
+          } else if (i === (uw - 1)) {
+            if (j === (uh - 1)) {
+              rect = cc.rect(this._cell.width * 2, 0, this._cell.width, this._cell.height);
+            } else if (j === 0) {
+              rect = cc.rect(this._cell.width * 2, this._cell.height * 2, this._cell.width, this._cell.height);
+            } else {
+              rect = cc.rect(this._cell.width * 2, this._cell.height, this._cell.width, this._cell.height);
+            }
+          } else if (j === (uh - 1)) {
+            rect = cc.rect(this._cell.width, 0, this._cell.width, this._cell.height);
+          } else if (j === 0) {
+            rect = cc.rect(this._cell.width, this._cell.height * 2, this._cell.width, this._cell.height);
+          } else {
+            rect = cc.rect(this._cell.width, this._cell.height, this._cell.width, this._cell.height);
+          }
+          rect.origin = cc.pAdd(rect.origin, this._cell.origin);
+          b = cc.Sprite.createWithTexture(this.getTexture(), rect);
+          b.setAnchorPoint(cc.p(0, 0));
+          if (j === (uh - 1) && i === (uw - 1)) {
+            b.setPosition(cc.p((i - 1) * this._cell.width + offw, (j - 1) * this._cell.height + offh));
+          } else if (j === (uh - 1)) {
+            b.setPosition(cc.p(i * this._cell.width, (j - 1) * this._cell.height + offh));
+          } else if (i === (uw - 1)) {
+            b.setPosition(cc.p((i - 1) * this._cell.width + offw, j * this._cell.height));
+          } else {
+            b.setPosition(cc.p(i * this._cell.width, j * this._cell.height));
+          }
+          b.setTag(j * this._cell.height + i);
+          _results1.push(this.addChild(b));
+        }
+        return _results1;
+      }).call(this));
+    }
+    return _results;
+  }
+});
+
+cpz.DecoratedBox.createWithTexture = function(texture, rect, size) {
+  var box;
+  box = new DecoratedBox();
+  if (box && box.initWithTexture(texture, rect, size)) {
+    return box;
+  }
+  return null;
+};
+
+cpz.DecoratedBox.createWithFile = function(filename, size) {
+  var box;
+  box = new DecoratedBox();
+  if (box && box.initWithFile(filename, size)) {
+    return box;
+  }
+  return null;
+};
