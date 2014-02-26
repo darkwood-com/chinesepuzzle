6f4df0e40081df16d1f69bbdc212b85f76dee233
Mathieu Ledru <matyo91@gmail.com>
2014-02-26 11:51:19 +0100

    png compress off

--- Mac/Src/ChinesePuzzle/AppController.mm
+++ Mac/Src/ChinesePuzzle/AppController.mm
@@ -42,7 +42,7 @@ USING_NS_CC;
 {
     if (self = [super init])
 	{
-		resolutions = [NSMutableDictionary dictionary];
+        
     }
 
     return self;
@@ -50,106 +50,6 @@ USING_NS_CC;
 
 - (void)dealloc
 {
-}
-
-- (NSMenuItem*) resolution480x320
-{
-	return [resolutions valueForKey:@"480x320"];
-}
-
-- (void) setResolution480x320:(NSMenuItem*) item
-{
-	return [resolutions setValue:item forKey:@"480x320"];
-}
-
-- (NSMenuItem*) resolution960x640
-{
-	return [resolutions valueForKey:@"960x640"];
-}
-
-- (void) setResolution960x640:(NSMenuItem*) item
-{
-	return [resolutions setValue:item forKey:@"960x640"];
-}
-
-- (NSMenuItem*) resolution1024x768
-{
-	return [resolutions valueForKey:@"1024x768"];
-}
-
-- (void) setResolution1024x768:(NSMenuItem*) item
-{
-	return [resolutions setValue:item forKey:@"1024x768"];
-}
-
-- (NSMenuItem*) resolution1280x800
-{
-	return [resolutions valueForKey:@"1280x800"];
-}
-
-- (void) setResolution1280x800:(NSMenuItem*) item
-{
-	return [resolutions setValue:item forKey:@"1280x800"];
-}
-
-- (NSMenuItem*) resolution1280x1024
-{
-	return [resolutions valueForKey:@"1280x1024"];
-}
-
-- (void) setResolution1280x1024:(NSMenuItem*) item
-{
-	return [resolutions setValue:item forKey:@"1280x1024"];
-}
-
-- (NSMenuItem*) resolution1366x768
-{
-	return [resolutions valueForKey:@"1366x768"];
-}
-
-- (void) setResolution1366x768:(NSMenuItem*) item
-{
-	return [resolutions setValue:item forKey:@"1366x768"];
-}
-
-- (NSMenuItem*) resolution1440x900
-{
-	return [resolutions valueForKey:@"1440x900"];
-}
-
-- (void) setResolution1440x900:(NSMenuItem*) item
-{
-	return [resolutions setValue:item forKey:@"1440x900"];
-}
-
-- (NSMenuItem*) resolution1680x1050
-{
-	return [resolutions valueForKey:@"1680x1050"];
-}
-
-- (void) setResolution1680x1050:(NSMenuItem*) item
-{
-	return [resolutions setValue:item forKey:@"1680x1050"];
-}
-
-- (NSMenuItem*) resolution1920x1080
-{
-	return [resolutions valueForKey:@"1920x1080"];
-}
-
-- (void) setResolution1920x1080:(NSMenuItem*) item
-{
-	return [resolutions setValue:item forKey:@"1920x1080"];
-}
-
-- (NSMenuItem*) resolution1920x1200
-{
-	return [resolutions valueForKey:@"1920x1200"];
-}
-
-- (void) setResolution1920x1200:(NSMenuItem*) item
-{
-	return [resolutions setValue:item forKey:@"1920x1200"];
 }
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
@@ -197,25 +97,6 @@ USING_NS_CC;
 //	{
 //		[[resolutions valueForKey:sRes] setState:NSOnState];
 //	}
-}
-
-- (IBAction)changeResolution:(NSMenuItem*)sender
-{
-	for(NSString* menuItemKey in resolutions)
-	{
-		NSMenuItem* menuItem = [resolutions valueForKey:menuItemKey];
-		if(menuItem == sender)
-		{
-//			AppDelegate* app = &dynamic_cast<AppDelegate&>(cocos2d::CCApplication::sharedApplication());
-//			std::string sRes = [menuItemKey cStringUsingEncoding:NSUTF8StringEncoding];
-//			app->getGameScene()->setResolution(sRes);
-			[menuItem setState:NSOnState];
-		}
-		else
-		{
-			[menuItem setState:NSOffState];
-		}
-	}
 }
 
 @end
