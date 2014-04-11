c38bb0895da1511977fdc9adf26be729b1303ec1
Mathieu Ledru <matyo91@gmail.com>
2014-04-11 17:34:04 +0200

    main

--- Browser/Project/main.js
+++ Browser/Project/main.js
@@ -25,76 +25,39 @@
  THE SOFTWARE.
  ****************************************************************************/
 
-/*var cocos2dApp = cc.Application.extend({
-    config:document['ccConfig'],
-    ctor:function () {
-        this._super();
-        cc.COCOS2D_DEBUG = this.config['COCOS2D_DEBUG'];
-        cc.initDebugSetting();
-        cc.setup(this.config['tag']);
-        cc.AppController.shareAppController().didFinishLaunchingWithOptions();
-    },
-    applicationDidFinishLaunching:function () {
-        var chinesePuzzle = null;
-
-        // initialize director
-        var director = cc.Director.getInstance();
-
-        //view resize
-        var view = cc.EGLView.getInstance();
-        var callback = function() {
-            var w = window,
-                d = document,
-                e = d.documentElement,
-                g = d.getElementsByTagName('body')[0],
-                x = w.innerWidth || e.clientWidth || g.clientWidth,
-                y = w.innerHeight|| e.clientHeight|| g.clientHeight;
-
-            view.setDesignResolutionSize(x, y, cc.RESOLUTION_POLICY.NO_BORDER);
-
-            if(chinesePuzzle) {
-                chinesePuzzle.reshape();
-            }
-        }
-        view.resizeWithBrowserSize(true);
-        view.setResizeCallback(callback);
-        callback.call();
-
-        //cc.EGLView.getInstance().setDesignResolutionSize(800,450,cc.RESOLUTION_POLICY.SHOW_ALL);
-
-        // enable High Resource Mode(2x, such as iphone4) and maintains low resource on other devices.
-        //director.enableRetinaDisplay(true);
-
-        // turn on display FPS
-        director.setDisplayStats(this.config['showFPS']);
-
-        // set FPS. the default value is 1.0/60 if you don't call this
-        director.setAnimationInterval(1.0 / this.config['frameRate']);
-
-        //load resources
-        cc.LoaderScene.preload(cpz.Resources, function () {
-            setTimeout(function() {
-                chinesePuzzle = cpz.GameScene.create();
-                director.replaceScene(chinesePuzzle);
-            }, 100);
-        }, this);
-
-        return true;
-    },
-    getCurrentLanguage: function() {
-        return cc.Application.getCurrentLanguage();
-    }
-});
-var myApp = new cocos2dApp();*/
-
 var cpz = cpz || {};
 cpz.CommonPath = '../../Common/Bin/Data/';
 cpz.CommonSrcPath = cpz.CommonPath + 'js/';
 
 cc.game.onStart = function(){
+    var chinesePuzzle = null;
+
+    //view resize
+    var view = cc.view;
+    var callback = function() {
+        var w = window,
+            d = document,
+            e = d.documentElement,
+            g = d.getElementsByTagName('body')[0],
+            x = w.innerWidth || e.clientWidth || g.clientWidth,
+            y = w.innerHeight|| e.clientHeight|| g.clientHeight;
+
+        view.setDesignResolutionSize(x, y, cc.ResolutionPolicy.NO_BORDER);
+
+        if(chinesePuzzle) {
+            chinesePuzzle.reshape();
+        }
+    };
+    view.resizeWithBrowserSize(true);
+    view.setResizeCallback(callback);
+    callback.call();
+
+    //view.setDesignResolutionSize(800,450,cc.ResolutionPolicy.SHOW_ALL);
+
     cc.LoaderScene.preload(cpz.Resources, function () {
         setTimeout(function() {
-            cc.director.runScene(cpz.GameScene.create());
+            chinesePuzzle = cpz.GameScene.create();
+            cc.director.runScene(chinesePuzzle);
         }, 100)
     }, this);
 };
