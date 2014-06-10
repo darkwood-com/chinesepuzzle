cf2186b0750ad644414c61e2e7dc3c6c6d70a042
Mathieu Ledru <matyo91@gmail.com>
2014-06-10 11:03:17 +0200

    android

--- Android/Project/jni/Android.mk
+++ Android/Project/jni/Android.mk
@@ -2,26 +2,33 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_MODULE := cocos2djs_shared
+LOCAL_MODULE := js_chinesepuzzle_shared
 
-LOCAL_MODULE_FILENAME := libcocos2djs
+LOCAL_MODULE_FILENAME := libjs_chinesepuzzle
 
-LOCAL_SRC_FILES := chinesepuzzle/main.cpp \
+LOCAL_SRC_FILES := main.cpp \
                    ../../../Common/Src/ChinesePuzzle/AppDelegate.cpp
 
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../../Common/Src/ChinesePuzzle
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../Classes
 
 LOCAL_WHOLE_STATIC_LIBRARIES := cocos_jsb_static
-LOCAL_WHOLE_STATIC_LIBRARIES += jsb_extension_static
 LOCAL_WHOLE_STATIC_LIBRARIES += jsb_chipmunk_static
+LOCAL_WHOLE_STATIC_LIBRARIES += jsb_extension_static
 LOCAL_WHOLE_STATIC_LIBRARIES += jsb_localstorage_static
+LOCAL_WHOLE_STATIC_LIBRARIES += jsb_network_static
+LOCAL_WHOLE_STATIC_LIBRARIES += jsb_builder_static
+LOCAL_WHOLE_STATIC_LIBRARIES += jsb_ui_static
+LOCAL_WHOLE_STATIC_LIBRARIES += jsb_studio_static
 
-LOCAL_EXPORT_CFLAGS := -DCOCOS2D_DEBUG=2 -DCOCOS2D_JAVASCRIPT
+LOCAL_EXPORT_CFLAGS := -DCOCOS2D_DEBUG=1
 
 include $(BUILD_SHARED_LIBRARY)
 
-
-$(call import-module,scripting/javascript/bindings)
-$(call import-module,scripting/javascript/bindings/extension)
-$(call import-module,scripting/javascript/bindings/chipmunk)
-$(call import-module,scripting/javascript/bindings/localstorage)
+$(call import-module,bindings)
+$(call import-module,bindings/manual/chipmunk)
+$(call import-module,bindings/manual/extension)
+$(call import-module,bindings/manual/localstorage)
+$(call import-module,bindings/manual/network)
+$(call import-module,bindings/manual/cocosbuilder)
+$(call import-module,bindings/manual/ui)
+$(call import-module,bindings/manual/cocostudio)
---
+++ Android/Project/src/org/cocos2dx/js_chinesepuzzle/AppActivity.java
@@ -0,0 +1,6 @@
+package org.cocos2dx.js_chinesepuzzle;
+
+import org.cocos2dx.lib.Cocos2dxActivity;
+
+public class AppActivity extends Cocos2dxActivity {
+}
