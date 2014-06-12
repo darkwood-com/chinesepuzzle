a4e5799ee3fec1e675663bb06b1fd6818ab9ddab
Mathieu Ledru <matyo91@gmail.com>
2014-06-12 16:58:12 +0200

    android

--- Android/Project/src/org/cocos2dx/js_chinesepuzzle/AppActivity.java
+++ Android/Project/src/org/cocos2dx/js_chinesepuzzle/AppActivity.java
@@ -1,6 +1,13 @@
 package org.cocos2dx.js_chinesepuzzle;
 
 import org.cocos2dx.lib.Cocos2dxActivity;
+import org.cocos2dx.lib.Cocos2dxGLSurfaceView;
 
 public class AppActivity extends Cocos2dxActivity {
+    public Cocos2dxGLSurfaceView onCreateView() {
+        Cocos2dxGLSurfaceView view = new Cocos2dxGLSurfaceView(this);
+        view.setEGLConfigChooser(8 , 8, 8, 8, 16, 8);
+
+        return view;
+    }
 }
