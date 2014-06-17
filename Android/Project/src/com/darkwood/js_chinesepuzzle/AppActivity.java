15cd0cfb6bd89e8b4b14c44d590cf68527d1bc20
Mathieu Ledru <matyo91@gmail.com>
2014-06-17 14:17:14 +0200

    androidstore

---
+++ Android/Project/src/com/darkwood/js_chinesepuzzle/AppActivity.java
@@ -0,0 +1,28 @@
+package org.cocos2dx.js_chinesepuzzle;
+
+import org.cocos2dx.lib.Cocos2dxActivity;
+import org.cocos2dx.lib.Cocos2dxGLSurfaceView;
+import android.os.Build;
+
+public class AppActivity extends Cocos2dxActivity {
+    public Cocos2dxGLSurfaceView onCreateView() {
+        Cocos2dxGLSurfaceView view = new Cocos2dxGLSurfaceView(this);
+
+        if (isAndroidEmulatorRunning())
+            view.setEGLConfigChooser(8 , 8, 8, 8, 16, 0);
+        else
+            view.setEGLConfigChooser(8 , 8, 8, 8, 16, 8);
+
+        return view;
+    }
+
+    private final static boolean isAndroidEmulatorRunning() {
+        String model = Build.MODEL;
+        String product = Build.PRODUCT;
+        boolean isEmulator = false;
+        if (product != null) {
+            isEmulator = product.equals("sdk") || product.contains("_sdk") || product.contains("sdk_");
+        }
+        return isEmulator;
+   }
+}
